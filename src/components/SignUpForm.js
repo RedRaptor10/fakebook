f9c05c7bc21049fd0f119ef12fed873cafb4492d
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-05 09:34:43 -0500

    Remove import React

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const SignUpForm = ({user, setShowSignUpForm}) => {
     const [signUpForm, setSignUpForm] = useState({
