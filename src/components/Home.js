aec738099bf60bd33afc8b6abb0996c553f995a0
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-05 09:36:01 -0500

    Move Log In to component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,78 +1,14 @@
-import React, { useState } from 'react';
-import SignUpForm from './SignUpForm';
+import Header from './Header';
+import LogInForm from './LogInForm';
 
 const Home = ({user, setUser}) => {
-    const [logInForm, setLogInForm] = useState({
-        email: '',
-        password: ''
-    });
-    const [logInErrors, setLogInErrors] = useState([]);
-    const [showSignUpForm, setShowSignUpForm] = useState(false);
-
-    const handleLogInChange = event => {
-        setLogInForm({
-            ...logInForm,
-            [event.target.name]: event.target.value
-        });
-    };
-
-    const submitLogIn = event => {
-        event.preventDefault();
-
-        const options = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                email: logInForm.email,
-                password: logInForm.password
-            }),
-            mode: 'cors'
-        };
-
-        fetch(process.env.REACT_APP_SERVER + 'api/log-in', options)
-        .then(function(res) { return res.json(); })
-        .then(function(res) {
-            if (res.errors) { setLogInErrors(res.errors); } // Email/password required
-            else if (!res.user) { setLogInErrors([{ msg: res.info.message }]); } // Incorrect email/password
-            else {
-                // Success. Set token as a cookie and set user
-                document.cookie = 'odinbook_api_token=' + res.token + '; SameSite=Lax; path=/';
-                setUser(res.user);
-            }
-        });
-    };
 
     return (
         <main id="home">
             {user ?
-                <div></div>
+                <Header setUser={setUser} />
             :
-                <div>
-                    <div>
-                        <h1>odinbook</h1>
-                        <h3>Connect with friends and the world around you on Odinbook.</h3>
-                    </div>
-                    <form id="log-in-form" action ="">
-                        <input type="text" name="email" placeholder="Email" onChange={handleLogInChange}></input>
-                        <input type="password" name="password" placeholder="Password" onChange={handleLogInChange}></input>
-                        <button type="submit" name="submit" onClick={submitLogIn}>Log In</button>
-                        {logInErrors.length !== 0 ?
-                            <ul id="form-errors">
-                                {logInErrors.map((logInError, i) => {
-                                    return(
-                                        <li key={i}>{logInError.msg}</li>
-                                    )
-                                })}
-                            </ul>
-                        : null}
-                        <button type="button" name="create" onClick={() => { setShowSignUpForm(true); }}>Create new account</button>
-                    </form>
-
-                    {showSignUpForm ?
-                        <SignUpForm user={user} setShowSignUpForm={setShowSignUpForm} />
-                    : null}
-
-                </div>
+                <LogInForm user={user} setUser={setUser} />
             }
         </main>
     );
