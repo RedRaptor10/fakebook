3326799c8cbb323586256dc13c4bd8bfdcba56c2
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-08 11:47:17 -0500

    Add Requests link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Sidebar = ({user}) => {
     return (
         <ul id="sidebar">
             <li>{user.firstName + ' ' + user.lastName}</li>
             <li>Friends</li>
+            <li><Link to="/friends/requests">Requests</Link></li>
         </ul>
     );
 };
