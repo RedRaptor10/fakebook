99e5a712d0c4b145f6b2b2d1860123552d3d4960
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-08 11:47:03 -0500

    Remove setUser

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,14 @@
 import Sidebar from './Sidebar';
 import Timeline from './Timeline';
 import Contacts from './Contacts';
-import LogInForm from './LogInForm';
 
-const Home = ({user, setUser}) => {
-
+const Home = ({user}) => {
     return (
-        user ?
-            <main id="home">
-                <Sidebar user={user} />
-                <Timeline />
-                <Contacts />
-            </main>
-        :
-            <LogInForm user={user} setUser={setUser} />
+        <main id="home">
+            <Sidebar user={user} />
+            <Timeline />
+            <Contacts />
+        </main>
     );
 };
 
