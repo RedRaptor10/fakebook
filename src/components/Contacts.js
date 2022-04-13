5f6450fba005b55b6842c495a874187342db8789
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-13 18:02:28 -0500

    Add friends to Contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,41 @@
-const Contacts = () => {
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getCookie } from '../helpers/cookies';
+
+const Contacts = ({user}) => {
+    const [contacts, setContacts] = useState();
+
+    // Get Friends
+    useEffect(() => {
+        let token = getCookie('odinbook_api_token');
+
+        const options = {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + token },
+            mode: 'cors'
+        };
+
+        fetch(process.env.REACT_APP_SERVER + 'api/users/' + user.username + '/get-friends', options)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(res) {
+            setContacts(res);
+        });
+    }, [user.username]);
+
     return (
         <div id="contacts">
-            Contacts
+            <h3>Contacts</h3>
+            {contacts ?
+                contacts.map(contact => {
+                    return (
+                        <Link key={contact._id} to={'/' + contact.username}>
+                            {contact.firstName + ' ' + contact.lastName}
+                        </Link>
+                    )
+                })
+            : null}
         </div>
     );
 };
