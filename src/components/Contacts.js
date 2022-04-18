981f82ffc8e2dc88d41ac695c1efdd3644cf4e72
RedRaptor10 <weiminh_lam@yahoo.com>
2022-04-18 17:55:03 -0500

    Stylized Contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -28,13 +28,24 @@ const Contacts = ({user}) => {
         <div id="contacts">
             <h3>Contacts</h3>
             {contacts ?
-                contacts.map(contact => {
-                    return (
-                        <Link key={contact._id} to={'/' + contact.username}>
-                            {contact.firstName + ' ' + contact.lastName}
-                        </Link>
-                    )
-                })
+                <ul id="contacts-list">
+                    {contacts.map(contact => {
+                        return (
+                            <li key={contact._id}>
+                                <Link to={'/' + contact.username}>
+                                    {contact.photo ?
+                                        <img className="profile-photo"
+                                            src={process.env.REACT_APP_SERVER + "/uploads/profile-photos/" + contact._id + "/" + contact.photo}
+                                            alt={contact.firstName + ' ' + contact.lastName} />
+                                    :
+                                        <img className="profile-photo" src={process.env.REACT_APP_SERVER + '/uploads/profile-photos/default.jpg'}
+                                            alt={contact.firstName + ' ' + contact.lastName} />}
+                                    <span>{contact.firstName + ' ' + contact.lastName}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
             : null}
         </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,17 @@ import { Link } from 'react-router-dom';
 const Sidebar = ({user}) => {
     return (
         <ul id="sidebar">
-            <li>{user.firstName + ' ' + user.lastName}</li>
+            <li>
+                <Link to={'/' + user.username}>
+                    {user.photo ?
+                        <img className="profile-photo" src={process.env.REACT_APP_SERVER + "/uploads/profile-photos/" + user._id + "/" + user.photo}
+                            alt={user.firstName + ' ' + user.lastName} />
+                    :
+                        <img className="profile-photo" src={process.env.REACT_APP_SERVER + '/uploads/profile-photos/default.jpg'}
+                            alt={user.firstName + ' ' + user.lastName} />}
+                    <span>{user.firstName + ' ' + user.lastName}</span>
+                </Link>
+            </li>
             <li><Link to="/users">Find Friends</Link></li>
             <li><Link to="/friends/requests">Requests</Link></li>
         </ul>
