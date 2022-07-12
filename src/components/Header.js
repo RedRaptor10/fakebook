fae841c710ac2beb3b05516e35c72f087e07ffa3
RedRaptor10 <weiminh_lam@yahoo.com>
2022-07-12 18:52:35 -0500

    Add search box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { deleteCookie } from '../helpers/cookies';
 import checkImage from '../helpers/checkImage';
 import defaultPhoto from '../assets/default-photo.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({user, setUser, darkMode, setDarkMode}) => {
+    const [searchInput, setSearchInput] = useState('');
     const navigate = useNavigate();
+
+    const handleInput = event => {
+        setSearchInput(event.target.value);
+    };
+
+    const submitSearch = event => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            navigate('/search/posts?q=' + searchInput);
+        }
+    };
+
+    const toggleDarkMode = () => {
+        darkMode ? setDarkMode(false) : setDarkMode(true);
+    };
 
     const logOut = () => {
         fetch(process.env.REACT_APP_SERVER + 'api/log-out', { mode: 'cors' })
@@ -17,15 +34,17 @@ const Header = ({user, setUser, darkMode, setDarkMode}) => {
         });
     };
 
-    const toggleDarkMode = () => {
-        darkMode ? setDarkMode(false) : setDarkMode(true);
-    };
-
     return (
         <header className={darkMode ? 'dark' : null}>
             <h1>
                 <Link to="/">fakebook</Link>
             </h1>
+            <div id="search-box">
+                <form>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    <input type="search" placeholder="Search" onChange={handleInput} onKeyDown={submitSearch}></input>
+                </form>
+            </div>
             <div id="header-items">
                 <div id="dark-mode-btn-container" onClick={toggleDarkMode}>
                     <button id="dark-mode-btn" type="button">
